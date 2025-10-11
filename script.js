06baf3f87d458d98c10429148043703b90b4017e
MatSifres <matisifres@gmail.com>
2025-10-11 02:17:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -64,8 +64,10 @@
         console.error('No se encontró LS.cart o el cartId');
       }
     } else {
-    */
+    
       console.log('Página recargada muy rápidamente, evitando envío duplicado');
     }
   });
 })();
+
+*/
